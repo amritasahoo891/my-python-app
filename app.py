8252936d8d0a655eb4d6dc762eb3d27e06bba455
amritasahoo891 <43074141+amritasahoo891@users.noreply.github.com>
2018-11-22 15:56:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-# redis = Redis(host='redis', port=6379)
+redis = Redis(host='redis')
+#redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
     # count = redis.incr('hits')
-    return 'Hello World! Im running in a docker Container - Version 0.0.2'
+    return 'Hello World! Im running in a docker Container - Version 0.0.2 redis'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
